f6e9d4f1f84f8a2749533837a90f0f06e4ef3e1c
travisbrigman <travis1000@gmail.com>
2021-03-01 13:49:35 -0600

    feat: added biometric ID in ContentView.swift

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -33,7 +33,7 @@ struct EditView: View {
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ") +
-                            Text("Page Description Here")
+                                Text(page.description)
                             .italic()
                         }
                     } else if loadingState == .loading {
@@ -60,7 +60,7 @@ struct EditView: View {
                 let decoder = JSONDecoder()
                 
                 if let items = try? decoder.decode(Result.self, from: data) {
-                    self.pages = Array(items.query.pages.values)
+                    self.pages = Array(items.query.pages.values).sorted()
                     self.loadingState = .loaded
                     return
                 }
--- BucketList/Result.swift
+++ BucketList/Result.swift
@@ -17,8 +17,16 @@ struct Query: Codable {
     let pages: [Int: Page]
 }
 
-struct Page: Codable {
+struct Page: Codable, Comparable {
     let pageid: Int
     let title: String
     let terms: [String: [String]]?
+    
+    var description: String {
+        terms?["description"]?.first ?? "No Description"
+    }
+    
+    static func < (lhs: Page, rhs: Page) -> Bool {
+        return lhs.title < rhs.title
+    }
 }
