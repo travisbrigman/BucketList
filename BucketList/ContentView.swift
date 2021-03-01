c5ddd6af05ebef924fb914c994970da7b4df0788
travisbrigman <travis1000@gmail.com>
2021-02-28 21:43:09 -0600

    feat: add touchID/faceID to ContentView.swift

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,18 +7,44 @@
 //
 
 import SwiftUI
-
+import LocalAuthentication
 
 
 struct ContentView: View {
-    
+    @State private var isUnlocked = false
     
     var body: some View {
-        MapView()
-            .edgesIgnoringSafeArea(.all)
+        VStack {
+            if self.isUnlocked {
+                Text("unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data"
             
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {     success, authenticationError in
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        //problem
+                    }
+                }
+            }
+        } else {
+            //no biometrics
         }
     }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
